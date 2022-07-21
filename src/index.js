bf85a7805131d677a1dc2f5fd2d680eca9bab38f
Pedro Correia <correia.pedro@outlook.com>
2022-07-21 15:36:54 -0300

    recebendo obejto no post e comparando cpf

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf: sting
+ * nome: string;
+ * id: uuid;
+ * statement: [];
+ */
+
+app.post("/conta", (request, response) => {
+  const { cpf, nome } = request.body;
+
+  // Busca e compara CPF
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    nome,
+    id,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
