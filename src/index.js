b6aade23a87a568f338912d5e3c0a3eeca22a8c5
Pedro Correia <correia.pedro@outlook.com>
2022-07-21 15:15:50 -0300

    usando paramentros e recebendo json no post

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-const { response } = require("express");
 const express = require("express");
-const { request } = require("http");
 
 /**
  * Tipos de parametros
  *
  * route params => identificar um recurso para editar/deletar/buscar
+ * query params => paginação / filtro   Exemplo url query params para paginação: localhost:3333/cursos?page=1&order=asc
+ * body params => Objetos para inserção / alteração
  */
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/cursos", (request, response) => {
+  const query = request.query;
+  console.log(query);
+
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/cursos", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
